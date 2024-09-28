d2e4c2f1f6e1912a0964f73c5f2e75995ce68f01
dlovell <dlovell@gmail.com>
2024-09-28 11:14:50 -0400

    fix: utils.writeShellScriptBinWithArgs: set filesystem friendly pname

--- nix/utils.nix
+++ nix/utils.nix
@@ -2,33 +2,36 @@
 rec {
   pname-or-name = drv: if drv ? "pname" then drv.pname else drv.name;
 
-  writeShellScriptBinWithArgs = { drv, name ? pname-or-name drv, args ? [ ], append-args ? true, set-eux ? true }: let
-    name' = if append-args then (builtins.concatStringsSep "-" ([ (prefix + name) ] ++ args)) else name;
+  replace-slash = string: pkgs.lib.replaceStrings [ "/" ] [ "-" ] string;
+
+  writeShellScriptBinWithArgs = { drv, bin-name ? pname-or-name drv, args ? [ ], append-args ? true, set-eux ? true }: let
+    name' = if append-args then (replace-slash (builtins.concatStringsSep "-" ([ (prefix + bin-name) ] ++ args))) else bin-name;
     preamble = if set-eux then "set -eux" else "";
     infix = builtins.concatStringsSep " " args;
-  in pkgs.writeShellScriptBin "${name'}" ''
+  in (pkgs.writeShellScriptBin name' ''
     ${preamble}
-    ${drv}/bin/${name} ${infix} "''${@}"
-  '';
+    ${drv}/bin/${bin-name} ${infix} "''${@}"
+  '').overrideAttrs {
+      pname = name';
+    };
 
   drvToApp = { drv, name ? pname-or-name drv }: {
     type = "app";
     program = "${drv}/bin/${name}";
   };
 
-  drvToAppWithArgs = { drv, name ? pname-or-name drv, args ? [ ] }: let
-    drvWithArgs = writeShellScriptBinWithArgs { inherit drv name args; };
+  drvToAppWithArgs = { drv, bin-name ? pname-or-name drv, args ? [ ] }: let
+    drvWithArgs = writeShellScriptBinWithArgs { inherit drv bin-name args; };
   in drvToApp { drv = drvWithArgs; };
 
 	mkNixFlakeMetadataRefresh = url: writeShellScriptBinWithArgs {
     drv = pkgs.nix;
     args = [ "flake" "metadata" "--refresh" url ];
-    name = "${prefix}nix-flake-metadata-refresh";
-    append-args = false;
+    append-args = true;
 	};
 
-	mkNixFlakeMetadataRefreshApp = url: drvToApp {
-		drv = mkNixFlakeMetadataRefresh url;
+  mkNixFlakeMetadataRefreshApp = url: drvToApp {
+    drv = mkNixFlakeMetadataRefresh url;
 	};
 
   attrsToApps = attrs: builtins.mapAttrs (name: drv: drvToApp { inherit drv; }) attrs;
